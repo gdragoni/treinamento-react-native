f7ee70be371a01d643c0db428ec1449fd64e84c7
Gabriel Dragoni <ga.dragoni@gmail.com>
2019-10-22 10:32:16 -0300

    hello world

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { 
+    View,
+    Text,
+    StyleSheet,
+ } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 15,
+        fontWeight: "bold",
+        color: "#649bcc",
+    }
+});
