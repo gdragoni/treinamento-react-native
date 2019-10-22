32d808d51488b33597085d0d6c68109f348fb992
Gabriel Dragoni <ga.dragoni@gmail.com>
2019-10-22 11:54:54 -0300

    Form1 with hooks

--- src/pages/FormPage1/index.js
+++ src/pages/FormPage1/index.js
@@ -9,8 +9,33 @@ import {
 import styles from './styles';
 
 export default function FormPage1({ navigation }) {
+    const [name, setName] = React.useState("");
+    const [age, setAge] = React.useState("");
+    const [email, setEmail] = React.useState("");
+
   return (
     <View style={styles.container}>
+        <View>
+            <Text>Nome</Text>
+            <TextInput
+                value={name}
+                onChangeText={value => setName(value)}
+            />
+        </View>
+        <View>
+            <Text>Idade</Text>
+            <TextInput
+                value={age}
+                onChangeText={value => setAge(value)}
+            />
+        </View>
+        <View>
+            <Text>E-mail</Text>
+            <TextInput
+                value={email}
+                onChangeText={value => setEmail(value)}
+            />
+        </View>
         <Button 
         title="Next"
         onPress={() => navigation.navigate('formPage2')}
